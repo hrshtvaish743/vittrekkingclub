c20f4ff02f9d0059abbecf4b56b9ebae29cfbf4d
Harshit <hrshtvaish743@gmail.com>
2016-01-16 18:17:20 +0530

    Gallery Completed,Code fixed

---
+++ gallery1.js
@@ -0,0 +1,14 @@
+    var cpo = [];
+        												cpo["_object"] ="cp_widget_98197aa3-998f-4b2d-b69f-a957017ec38a";
+        												cpo["_fid"] = "AQEAAP9891Ya";
+        												var _cpmp = _cpmp || [];
+        												_cpmp.push(cpo);
+        												(function()
+        												{
+        													var cp = document.createElement("script"); cp.type = "text/javascript";
+        													cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+        													var c = document.getElementsByTagName("script")[0];
+        													c.parentNode.insertBefore(cp, c);
+        												}
+        												)
+        												();
