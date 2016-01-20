793d294d806048540f0cd99a6d8e8eb947869c29
Harshit <hrshtvaish743@gmail.com>
2016-01-21 00:37:21 +0530

    added reporting

--- events.php
+++ events.php
@@ -88,7 +88,7 @@
                                         $result = $conn->query($sql);
                                         if ($result->num_rows > 0) {
                                             while($row = $result->fetch_assoc()) {
-                                                echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+                                                echo "id: " . $row["id"]. " - Name: " . $row["conductor_name"] . "<br>". $row["event_name"] . "<br>" . $row["date"] . "<br>". $row["report"] . "<br>";
                                             }
                                         } else {
                                             echo "0 results";
